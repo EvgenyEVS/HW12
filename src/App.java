e0eb2c8d2c98c0212fe2855c3964c53670a11161
EvgenyEVS <evis10@yandex.ru>
2025-07-23 22:59:19 +0300

    HW 12.1

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+
+        Author levTolstoy = new Author("Лев","Толстой");
+        Author julesVerne = new Author("Jules","Verne");
+
+        Book warAndPeace = new Book("Война и мир",1886, levTolstoy);
+        Book mysteriousIsland = new Book("Mysterious Island", 2021, julesVerne);
+
+        System.out.println("Удивительное погружение в эпоху войн против Наполеона, \n" +
+                "со сложным сюжетом и тонкими переживаниями. Настоящая русская классика! \n" +
+                "Рекомендуем книгу " + levTolstoy.getAuthorName() + " " + levTolstoy.getAuthorSurname() + " " + warAndPeace.getBookName()
+                + ", впервые издана в " + warAndPeace.getPublicationYear() + " году.");
+
+        System.out.println();
+        System.out.println(mysteriousIsland.getBookName() + " - это увлекательные приключения. " + julesVerne.getAuthorName() + " " + julesVerne.getAuthorSurname()
+        + " как всегда \nпомещает своих героев в необычные условия, на этот раз - " + mysteriousIsland.getBookName() + "!");
+
+        mysteriousIsland.setPublicationYear(2024);
+        System.out.println();
+        System.out.println("Книга " + mysteriousIsland.getBookName() + " переиздана в " + mysteriousIsland.getPublicationYear() +
+                " году" + " с красочными картинками для детей.");
+
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private String bookName;
+    private int publicationYear;
+    private Author author;
+
+    public Book(String bookName, int publicationYear, Author author){
+        this.bookName = bookName;
+        this.publicationYear = publicationYear;
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
